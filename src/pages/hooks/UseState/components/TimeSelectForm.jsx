56f27a3ba9c4ac6a48592e83f195f0a18550cbc5
htring1314 <htring@outlook.com>
2020-04-06 21:05:55 +0800

    :mouse2: 0406

--- src/pages/hooks/UseState/components/TimeSelectForm.jsx
+++ src/pages/hooks/UseState/components/TimeSelectForm.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
-import { TimePicker, Modal, Form } from 'antd';
+import { TimePicker, Modal, Form, Select, Row, Col } from 'antd';
+import { formatMessage } from 'umi-plugin-react/locale';
+import { everyWeek, everyMonth, weeksSelect, daySelect } from './data';
+
+const { Option } = Select;
 
 // 时间选择格式：时、分
 const timeFormat = 'HH:mm';
@@ -21,6 +25,7 @@ function TimeSelectForm(props) {
     record = {},
     form: { getFieldDecorator },
     modalTitle,
+    selectCycle,
   } = props;
 
   // const [startTime, setStartTime] = useState(null);
@@ -37,25 +42,52 @@ function TimeSelectForm(props) {
   };
 
   const submitTime = () => {
-    const { form, onSubmit } = props;
+    const { form, onSaveTimeRange } = props;
     form.validateFields((err, values) => {
       if (err) {
+        // console.log('keys', Object.keys(err).includes('endTime'));
+        // console.log('出错了', err.endTime.errors[0].message);
         return;
       }
+      // console.log(values);
       if (values.startTime > values.endTime) {
-        onSubmit({ startTime: values.endTime, endTime: values.startTime });
+        onSaveTimeRange({
+          selectDay: '',
+          ...values,
+          startTime: values.endTime,
+          endTime: values.startTime,
+        });
       } else {
-        onSubmit(values);
+        onSaveTimeRange({ selectDay: '', ...values });
       }
     });
   };
+
+  const generateSelect = (arr = []) => {
+    if (!!arr && arr.length > 0) {
+      return arr.map(item => <Option key={item.value}>{item.name}</Option>);
+    }
+    return null;
+  };
+
+  const options = value => {
+    if (value === everyWeek.value) {
+      return generateSelect(weeksSelect);
+    }
+    if (value === everyMonth.value) {
+      return generateSelect(daySelect());
+    }
+    return null;
+  };
+
+  const needSelectDay = selectCycle === everyWeek.value || selectCycle === everyMonth.value;
 
   return (
     <div>
       <Modal
         visible={timeVisible}
         title={modalTitle}
-        okText="保存"
+        okText={formatMessage({ id: 'common.save' })}
         onCancel={onCancel}
         onOk={submitTime}
         closable
@@ -64,15 +96,37 @@ function TimeSelectForm(props) {
         width={600}
       >
         <Form style={{ marginTop: 8 }}>
-          <Form.Item {...formItemLayout} label="时间范围">
+          {needSelectDay ? (
+            <Form.Item {...formItemLayout} label={formatMessage({ id: 'common.select' })}>
+              {getFieldDecorator('selectDay', {
+                initialValue: record.selectDay,
+                rules: [{ required: true, message: formatMessage({ id: 'common.must' }) }],
+              })(
+                <Select placeholder={formatMessage({ id: 'common.please.select' })}>
+                  {options(selectCycle)}
+                </Select>,
+              )}
+            </Form.Item>
+          ) : null}
+          <Form.Item {...formItemLayout} label={formatMessage({ id: 'useState.range.time' })}>
             {getFieldDecorator('startTime', {
               initialValue: record.startTime,
-              rules: [{ required: true, message: '开始结束时间必输' }],
+              rules: [
+                {
+                  required: true,
+                  message: formatMessage({ id: 'useState.startEnd.required' }),
+                },
+              ],
             })(<TimePicker format={timeFormat} minuteStep={15} secondStep={60} />)}
             ~
             {getFieldDecorator('endTime', {
               initialValue: record.endTime,
-              rules: [{ required: true, message: '开始结束时间必输' }],
+              rules: [
+                {
+                  required: true,
+                  message: formatMessage({ id: 'useState.startEnd.required' }),
+                },
+              ],
             })(<TimePicker format={timeFormat} minuteStep={15} secondStep={60} />)}
           </Form.Item>
         </Form>
--- src/pages/hooks/UseState/index.jsx
+++ src/pages/hooks/UseState/index.jsx
@@ -5,6 +5,7 @@ import { Button, Form, DatePicker, Select, Row, Col, List } from 'antd';
 import moment from 'moment';
 import TimeSelectForm from './components/TimeSelectForm';
 import styles from './index.less';
+import { frequencySelect } from './components/data';
 
 const { Option } = Select;
 
@@ -22,31 +23,37 @@ const formItemLayout = {
   },
 };
 
-const radioArr = [
-  { value: '1', name: formatMessage({ id: 'useState.everyday' }) },
-  { value: '2', name: formatMessage({ id: 'useState.every.week' }) },
-  { value: '3', name: formatMessage({ id: 'useState.every.month' }) },
-];
-
 function UseState(props) {
+  // 本组件中props
   const {
     form: { getFieldDecorator },
   } = props;
 
+  // 是否表单提交中
   const [submiting, setSubmiting] = useState(false);
+  // 是否显示新增时间范围按钮
   const [selectDisplay, setSelectDisplay] = useState({ display: 'none' });
+  // 动态控制周期选择框的长度
   const [selectSpan, setSelectSpan] = useState(24);
+  // 录入的时间范围数据
   const [selectedTimes, setSelectedTimes] = useState([]);
+  // 是否显示时间范围新增弹框
   const [timeVisible, setTimeVisible] = useState(false);
+  // 编辑时间弹框时的数据
   const [timeRecord, setTimeRecord] = useState({});
+  // 时间弹框的标题
   const [modalTitle, setModalTitle] = useState('');
+  // 选中的周期
+  const [selectCycle, setSelectCycle] = useState('');
 
+  // 自动提交表单数据
   const handleSubmit = e => {
     setSubmiting(true);
     const { form } = props;
     e.preventDefault();
     form.validateFieldsAndScroll((err, values) => {
       if (!err) {
+        // console.log(selectedTimes);
         setTimeout(() => {
           setSubmiting(false);
         }, 1000);
@@ -54,6 +61,7 @@ function UseState(props) {
     });
   };
 
+  // 手动提交表单数据
   const manSubmit = () => {
     setSubmiting(true);
     const { form } = props;
@@ -66,39 +74,63 @@ function UseState(props) {
     });
   };
 
+  // 选择周期
   const selectType = value => {
-    if (value === '1') {
+    setSelectedTimes([]);
+    if (
+      frequencySelect.some(item => {
+        if (item.value === value) {
+          return true;
+        }
+        return false;
+      })
+    ) {
+      setSelectCycle(value);
       setSelectDisplay({ display: 'block' });
       setSelectSpan(20);
     } else {
+      setSelectCycle('');
       setSelectDisplay({ display: 'none' });
       setSelectSpan(24);
     }
   };
 
+  // 新增时间范围弹框
   const addDuty = () => {
     setTimeVisible(true);
     setModalTitle(formatMessage({ id: 'useState.add.time' }));
   };
 
+  // 关闭时间选择弹框
   const onCancel = () => {
+    // setSelectCycle('');
     setTimeVisible(false);
     setTimeRecord({});
   };
-  const onSubmit = params => {
+
+  // 保存新增的时间范围信息
+  const onSaveTimeRange = params => {
     setSelectedTimes(prev => {
-      (prev || []).push(params);
-      return prev;
+      const filterArr = (prev || []).filter(
+        item =>
+          item.selectDay !== timeRecord.selectDay &&
+          item.startTime !== timeRecord.startTime &&
+          item.endTime !== timeRecord.endTime,
+      );
+      filterArr.push(params);
+      return filterArr;
     });
     onCancel();
   };
 
+  // 编辑时间范围
   const editTime = item => {
     setTimeVisible(true);
     setTimeRecord(item);
     setModalTitle(formatMessage({ id: 'useState.edit.time' }));
   };
 
+  // 删除时间范围
   const delTime = item => {
     setSelectedTimes(prev =>
       (prev || []).filter(
@@ -115,20 +147,29 @@ function UseState(props) {
     >
       <div style={{ paddingTop: 15 }}>
         <Form onSubmit={handleSubmit} style={{ marginTop: 8 }}>
-          <Form.Item {...formItemLayout} label="开始结束日期">
+          <Form.Item {...formItemLayout} label={formatMessage({ id: 'useState.startEnd' })}>
             {getFieldDecorator('startDate')(
-              <DatePicker placeholder="请选择开始日期" style={{ width: '50%' }} />,
+              <DatePicker
+                placeholder={formatMessage({ id: 'useState.start.select' })}
+                style={{ width: '50%' }}
+              />,
             )}
             {getFieldDecorator('endDate')(
-              <DatePicker placeholder="请选择结束日期" style={{ width: '50%' }} />,
+              <DatePicker
+                placeholder={formatMessage({ id: 'useState.start.select' })}
+                style={{ width: '50%' }}
+              />,
             )}
           </Form.Item>
-          <Form.Item {...formItemLayout} label="选类型">
+          <Form.Item {...formItemLayout} label={formatMessage({ id: 'useState.frequency.select' })}>
             <Row>
               <Col span={selectSpan}>
                 {getFieldDecorator('troubleType', { rules: [{ required: true }] })(
-                  <Select onChange={selectType} placeholder="请选择类型">
-                    {radioArr.map(item => (
+                  <Select
+                    onChange={selectType}
+                    placeholder={formatMessage({ id: 'useState.frequency.please' })}
+                  >
+                    {frequencySelect.map(item => (
                       <Option key={item.value}>{item.name}</Option>
                     ))}
                   </Select>,
@@ -136,7 +177,7 @@ function UseState(props) {
               </Col>
               <Col span={4} style={{ display: selectDisplay.display }}>
                 <Button onClick={() => addDuty()} icon="plus">
-                  新增时间
+                  {formatMessage({ id: 'useState.range.add' })}
                 </Button>
               </Col>
             </Row>
@@ -146,26 +187,27 @@ function UseState(props) {
               itemLayout="horizontal"
               dataSource={selectedTimes}
               style={{ marginLeft: '29.2%' }}
-              renderItem={item => (
+              renderItem={(item, index) => (
                 <List.Item
                   actions={[
                     <a href="#" onClick={() => editTime(item)}>
-                      编辑
+                      {formatMessage({ id: 'common.edit' })}
                     </a>,
                     <a href="#" onClick={() => delTime(item)}>
-                      删除
+                      {formatMessage({ id: 'common.delete' })}
                     </a>,
                   ]}
                 >
-                  开始结束时间：{item.startTime.format(timeFormat)}~
-                  {item.endTime.format(timeFormat)}
+                  {`${formatMessage({ id: 'useState.startEnd.time' })} ${index + 1} :
+                  ${item.selectDay} ${item.startTime.format(timeFormat)}~
+                ${item.endTime.format(timeFormat)}`}
                 </List.Item>
               )}
             ></List>
           ) : null}
           <Form.Item style={{ textAlign: 'center' }}>
             <Button type="primary" htmlType="submit">
-              是时候
+              {formatMessage({ id: 'common.auto' })}
             </Button>
             <Button
               loading={submiting}
@@ -173,16 +215,17 @@ function UseState(props) {
               onClick={() => manSubmit()}
               className={styles.rightButton}
             >
-              手动提交
+              {formatMessage({ id: 'common.man' })}
             </Button>
           </Form.Item>
         </Form>
         <TimeSelectForm
           timeVisible={timeVisible}
           onCancel={onCancel}
-          onSubmit={onSubmit}
+          onSaveTimeRange={onSaveTimeRange}
           record={timeRecord}
           modalTitle={modalTitle}
+          selectCycle={selectCycle}
         ></TimeSelectForm>
       </div>
     </PageHeaderWrapper>
